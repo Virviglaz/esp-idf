3bae377b6f03c884418dfc2e7239aeeb9b822a55
Pavel Nadein <pavelnadein@gmail.com>
2023-04-07 23:32:55 +0200

    [i2c] i2c driver updated
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- components/drivers/include/esp32_i2c.h
+++ components/drivers/include/esp32_i2c.h
@@ -4,7 +4,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2022 Pavel Nadein
+ * Copyright (c) 2023 Pavel Nadein
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -42,42 +42,378 @@
  * Pavel Nadein <pavelnadein@gmail.com>
  */
 
-#ifndef __ESP32_I2C_H__
-#define __ESP32_I2C_H__
+/* FreeRTOS */
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <esp_log.h>
 
-#include <stdint.h>
+/* IDF Drivers */
+#include <driver/gpio.h>
 #include "driver/i2c.h"
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/semphr.h"
-
-enum i2c_freq {
-	I2C_FREQ_100K = 100000UL,
-	I2C_FREQ_400K = 400000UL,
-	I2C_FREQ_800K = 800000UL,
-};
-
-class i2c
+/**
+ * @brief I2c driver for ESP32.
+ */
+template <typename R = uint8_t> class i2c
 {
 public:
-	i2c(int sda_pin, int scl_pin, enum i2c_freq freq = I2C_FREQ_100K,
-		bool pullup = true, int bus_num = 0);
-	~i2c();
+	/**
+	 * @brief Default constructor.
+	 *
+	 * Call i2c::init() to use I2c.
+	 */
+	i2c() { }
 
-	esp_err_t write(uint8_t addr, uint8_t *reg, uint16_t reg_size,
-	uint8_t *buf, uint16_t size);
+	/**
+	 * @brief Initialize the I2c bus.
+	 *
+	 * @param[in] sda_pin		Number of SDA pin.
+	 * @param[in] scl_pin		Number of SCL pin.
+	 * @param[in] freq		Bus frequency. Typically 100..400kHz.
+	 * @param[in] pullup		Internal pullup enable. Default = yes.
+	 */
+	i2c(int sda_pin,
+	    int scl_pin,
+	    unsigned long freq = 100000UL,
+	    bool pullup = false,
+	    int bus_num = 0)
+	{
+		init_done =
+			init(sda_pin, scl_pin, freq, pullup, bus_num) == ESP_OK;
+	}
 
-	esp_err_t write_reg(uint8_t addr, uint8_t reg,
-		uint8_t *buf, uint16_t size);
+	/**
+	 * @brief Destroy the i2c::i2c object
+	 */
+	~i2c()
+	{
+		if (init_done) {
+			vSemaphoreDelete(lock);
+			i2c_driver_delete(used_bus_num);
+		}
+	}
 
-	esp_err_t read_reg(uint8_t addr, uint8_t reg,
-		uint8_t *buf, uint16_t size);
+	/**
+	 * @brief  Write data from the buffer to device register reg.
+	 *
+	 * @param[in] addr		I2c device address.
+	 * @param[in] reg		Pointer to register value.
+	 * @param[in] reg_size		Size of register value.
+	 * @param[in] buf		Pointer to data buffer.
+	 * @param[in] size		Buffer size.
+	 *
+	 * @retval			Zero if success, error if failed.
+	*/
+	esp_err_t write(uint8_t addr,
+			uint8_t *reg,
+			uint16_t reg_size,
+			uint8_t *buf,
+			uint16_t size)
+	{
+		esp_err_t res;
+		i2c_cmd_handle_t handle;
 
-	const char *get_bus_name();
+		if (!init_done)
+			return no_init_error(); 
+
+		if (xSemaphoreTake(lock, portMAX_DELAY) != pdTRUE)
+			return ESP_FAIL;
+
+		handle = i2c_cmd_link_create();
+		if (!handle) {
+			xSemaphoreGive(lock);
+			return ESP_ERR_NO_MEM;
+		}
+
+		res = i2c_master_start(handle);
+		if (res) {
+			ESP_LOGE(__FILE__, "start error: %s",
+				esp_err_to_name(res));
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		res = i2c_master_write_byte(handle,
+			(addr << 1) | I2C_MASTER_WRITE, true);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C write address error: %s",
+				esp_err_to_name(res));
+			i2c_master_stop(handle);
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		res = i2c_master_write(handle, reg, reg_size, I2C_MASTER_NACK);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C write error: %s",
+				esp_err_to_name(res));
+			i2c_master_stop(handle);
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		if (buf && size) {
+			res = i2c_master_write(handle, buf, size, false);
+			if (res) {
+				ESP_LOGE(__FILE__, "I2C write error: %s",
+					esp_err_to_name(res));
+				i2c_master_stop(handle);
+				i2c_cmd_link_delete(handle);
+				xSemaphoreGive(lock);
+				return res;
+			}
+		}
+
+		res = i2c_master_stop(handle);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C stop error: %s",
+				esp_err_to_name(res));
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		res = i2c_master_cmd_begin(used_bus_num, handle, portMAX_DELAY);
+		if (res)
+			ESP_LOGE(__FILE__, "I2C reading failed: %s",
+				esp_err_to_name(res));
+
+		i2c_cmd_link_delete(handle);
+		xSemaphoreGive(lock);
+		return ESP_OK;
+	}
+
+	/**
+	 * @brief  Read data from the device reg to buffer.
+	 *
+	 * @param[in] addr		I2c device address.
+	 * @param[in] reg		Pointer to register value.
+	 * @param[in] reg_size		Size of register value.
+	 * @param[out] buf		Pointer to destanation buffer.
+	 * @param[in] size		Buffer size.
+	 *
+	 * @retval			Zero if success, error if failed.
+	*/
+	esp_err_t read(uint8_t addr,
+		       uint8_t *reg,
+		       uint16_t reg_size,
+		       uint8_t *buf,
+		       uint16_t size)
+	{
+		esp_err_t res;
+		i2c_cmd_handle_t handle;
+
+		if (!init_done)
+			return no_init_error(); 
+
+		xSemaphoreTake(lock, portMAX_DELAY);
+
+		handle = i2c_cmd_link_create();
+		if (!handle) {
+			ESP_LOGE(__FILE__, "driver install failed: no memory");
+			xSemaphoreGive(lock);
+			return ESP_ERR_NO_MEM;
+		}
+
+		res = i2c_master_start(handle);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C start error: %s",
+				esp_err_to_name(res));
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		/* write address */
+		res = i2c_master_write_byte(handle,
+			(addr << 1) | I2C_MASTER_WRITE, true);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C write address error: %s",
+				esp_err_to_name(res));
+			i2c_master_stop(handle);
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		/* write destanation */
+		res = i2c_master_write(handle, reg, reg_size, I2C_MASTER_NACK);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C write byte error: %s",
+				esp_err_to_name(res));
+			i2c_master_stop(handle);
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		/* repeated start */
+		res = i2c_master_start(handle);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C repeated start error: %s",
+				esp_err_to_name(res));
+			i2c_master_stop(handle);
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		/* write address for reading */
+		res = i2c_master_write_byte(handle,
+			(addr << 1) | I2C_MASTER_READ, true);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C address write error: %s",
+				esp_err_to_name(res));
+			i2c_master_stop(handle);
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+		
+		if (buf && size > 1) {
+			res = i2c_master_read(handle, buf, size - 1,
+				I2C_MASTER_ACK);
+			if (res) {
+				ESP_LOGE(__FILE__, "I2C reading error: %s",
+					esp_err_to_name(res));
+				i2c_master_stop(handle);
+				i2c_cmd_link_delete(handle);
+				xSemaphoreGive(lock);
+				return res;
+			}
+			buf += size - 1;
+			size = 1;
+		}
+
+		/* last byte read with NACK */
+		if (buf && size) {
+			res = i2c_master_read(handle, buf, 1, I2C_MASTER_NACK);
+			if (res) {
+				ESP_LOGE(__FILE__, "I2C reading error: %s",
+					esp_err_to_name(res));
+				i2c_master_stop(handle);
+				i2c_cmd_link_delete(handle);
+				xSemaphoreGive(lock);
+				return res;
+			}
+		}
+
+		res = i2c_master_stop(handle);
+		if (res) {
+			ESP_LOGE(__FILE__, "I2C stop error: %s",
+				esp_err_to_name(res));
+			i2c_cmd_link_delete(handle);
+			xSemaphoreGive(lock);
+			return res;
+		}
+
+		res = i2c_master_cmd_begin(used_bus_num, handle, portMAX_DELAY);
+		if (res)
+			ESP_LOGE(__FILE__, "I2C reading failed: %s",
+				esp_err_to_name(res));
+		i2c_cmd_link_delete(handle);
+		xSemaphoreGive(lock);
+		return res;
+	}
+
+	esp_err_t init(int sda_pin,
+	    	       int scl_pin,
+		       unsigned long freq = 100000UL,
+		       bool pullup = false,
+		       int bus_num = 0)
+	{
+		esp_err_t res;
+
+		if (init_done) {
+			ESP_LOGE(__FILE__, "Already initialized");
+			return ESP_ERR_INVALID_STATE;
+		}
+
+		if (bus_num >= I2C_NUM_MAX) {
+			ESP_LOGE(__FILE__, "No more I2c busses available");
+			return ESP_ERR_INVALID_ARG;
+		}
+
+		i2c_config_t cfg;
+		cfg.mode = I2C_MODE_MASTER;
+		cfg.sda_io_num = sda_pin;
+		cfg.sda_pullup_en = pullup;
+		cfg.scl_io_num = scl_pin;
+		cfg.scl_pullup_en = pullup;
+		cfg.master.clk_speed = freq;
+		cfg.clk_flags = 0;
+
+		gpio_reset_pin((gpio_num_t)sda_pin);
+		gpio_reset_pin((gpio_num_t)scl_pin);
+
+		res = i2c_param_config(bus_num, &cfg);
+		if (res) {
+			ESP_LOGE(__FILE__, "configuration failed: %s",
+				esp_err_to_name(res));
+			return res;
+		}
+
+		res = i2c_driver_install(bus_num, cfg.mode, 0, 0, 0);
+		if (res) {
+			ESP_LOGE(__FILE__, "driver install failed: %s",
+				esp_err_to_name(res));
+			return res;
+		}
+
+		lock = xSemaphoreCreateMutex();
+		used_bus_num = bus_num;
+		init_done = lock ? true : false;
+
+		return init_done ? ESP_OK : ESP_ERR_NO_MEM;
+	}
+
+	/**
+	 * @brief  Write data to the device reg from buffer.
+	 *
+	 * @param[in] addr		I2c device address.
+	 * @param[in] reg		Register address value.
+	 * @param[in] buf		Pointer to data buffer.
+	 * @param[in] size		Buffer size.
+	 *
+	 * @retval			Zero if success, error if failed.
+	*/
+	esp_err_t write_reg(uint8_t addr,
+			    R reg,
+			    uint8_t *buf,
+			    uint16_t size)
+	{
+		return write(addr, &reg, sizeof(reg), buf, size);
+	}
+
+	/**
+	 * @brief  Read data from the device reg to buffer.
+	 *
+	 * @param[in] addr		I2c device address.
+	 * @param[in] reg		Register address value.
+	 * @param[in] buf		Pointer to data buffer.
+	 * @param[in] size		Buffer size.
+	 *
+	 * @retval			Zero if success, error if failed.
+	*/
+	esp_err_t read_reg(uint8_t addr,
+			   R reg,
+			   uint8_t *buf,
+			   uint16_t size)
+	{
+		return read(addr, &reg, sizeof(reg), buf, size);
+	}
+
 private:
-	i2c_port_t bus;
-	char bus_name[5];
 	SemaphoreHandle_t lock;
-};
+	bool init_done = false;
+	int used_bus_num;
 
-#endif /* __ESP32_I2C_H__ */
+	esp_err_t no_init_error()
+	{
+		ESP_LOGE(__FILE__, "I2c not initialized");
+		return ESP_ERR_INVALID_STATE;
+	}
+};
