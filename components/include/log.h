6b429306c4d67734188a57dfc295fc5b7d0fd509
Pavel Nadein <pavelnadein@gmail.com>
2023-11-11 19:06:45 +0100

    [log] make possible produce logs with unique tags
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- components/include/log.h
+++ components/include/log.h
@@ -3,9 +3,18 @@
 
 /* Logs */
 #include "esp_log.h"
+
+#ifdef UNIQ_LOGGING
+extern char *log_tag;
+#define DEBUG(format, ...)	ESP_LOGD(log_tag, format, ##__VA_ARGS__)
+#define INFO(format, ...)	ESP_LOGI(log_tag, format, ##__VA_ARGS__)
+#define WARN(format, ...)	ESP_LOGW(log_tag, format, ##__VA_ARGS__)
+#define ERROR(format, ...)	ESP_LOGE(log_tag, format, ##__VA_ARGS__)
+#else
 #define DEBUG(format, ...)	ESP_LOGD(__func__, format, ##__VA_ARGS__)
 #define INFO(format, ...)	ESP_LOGI(__func__, format, ##__VA_ARGS__)
 #define WARN(format, ...)	ESP_LOGW(__func__, format, ##__VA_ARGS__)
 #define ERROR(format, ...)	ESP_LOGE(__func__, format, ##__VA_ARGS__)
+#endif
 
 #endif /* __LOG_H__ */
