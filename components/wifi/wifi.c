a91f0e23008b21470097be5a2bde651145cdf412
Pavel Nadein <pavelnadein@gmail.com>
2024-02-10 23:22:50 +0100

    [wifi] improve wifi reconnect function
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- components/wifi/wifi.c
+++ components/wifi/wifi.c
@@ -5,15 +5,16 @@
 #include "lwip/sockets.h"
 #include <stdint.h>
 #include <string.h>
+#include <errno.h>
 #include <wifi.h>
 
-static const char *tag = "wifi";
+static char *tag = "wifi";
 static bool init_done = false;
 static bool is_connected = false;
 static const char *current_uuid;
-static bool scan_failed = false;
 static uint8_t mac[6];
 static char ip_address[16];
+static esp_netif_t *sta_netif = NULL;
 
 static struct {
 	wifi_credentials_t *ap_list;
@@ -43,7 +44,7 @@ static void event_start_scan(void)
 
 }
 
-static void event_find_ap_from_list(void)
+static int event_find_ap_from_list(void)
 {
 	uint16_t ap_found;
 	wifi_ap_record_t *ap_list;
@@ -54,15 +55,13 @@ static void event_find_ap_from_list(void)
 
 	if (ap_found <= 0) {
 		ESP_LOGD(tag, "No APs found, retry...");
-		event_start_scan();
-		return;
+		return EAGAIN;
 	}
 
 	ap_alloc = malloc(ap_found * sizeof(*ap_list));
 	if (!ap_alloc) {
 		ESP_LOGE(tag, "Memory low, retry later...");
-		event_start_scan();
-		return;
+		return ENOMEM;
 	}
 
 	ap_list = ap_alloc;
@@ -96,9 +95,10 @@ done:
 	if (is_found) {
 		ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_cfg));
 		ESP_ERROR_CHECK(esp_wifi_connect());
-	} else {
-		scan_failed = true;
+		return 0;
 	}
+
+	return EFAULT;
 }
 
 static void event_handler(void* arg, esp_event_base_t event_base,
@@ -115,7 +115,10 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 			return;
 		case WIFI_EVENT_SCAN_DONE:		/* Step 2 */
 			ESP_LOGD(tag, "Scanning is done, looking for the AP");
-			event_find_ap_from_list();
+			if (event_find_ap_from_list()) {
+				is_connected = false;
+				event_start_scan();
+			}
 			return;
 		case WIFI_EVENT_STA_CONNECTED:		/* Step 3 */
 			ESP_LOGD(tag, "Connection established.");
@@ -126,8 +129,6 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 		default: /* TODO: remove this */
 			ESP_LOGE(tag, "Undefined event %d, reconnecting...",
 				(int)event_id);
-			if (esp_wifi_connect() == ESP_ERR_WIFI_NOT_STARTED)
-				esp_wifi_start();
 			return;
 		}
 	}
@@ -178,17 +179,6 @@ int connect_to_server_from_list(int *socketfd,
 	return res;
 }
 
-static void monitor(void *arg)
-{
-	while (1) {
-		vTaskDelay(pdMS_TO_TICKS(10000));
-		if (scan_failed) {
-			event_start_scan();
-			scan_failed = false;
-		}
-	}
-}
-
 void wifi_start(wifi_credentials_t *ap_list, int size)
 {
 	const wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
@@ -202,7 +192,8 @@ void wifi_start(wifi_credentials_t *ap_list, int size)
 	ESP_ERROR_CHECK(nvs_flash_init());
 	ESP_ERROR_CHECK(esp_netif_init());
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
-	esp_netif_create_default_wifi_sta();
+	if (!sta_netif)
+		sta_netif = esp_netif_create_default_wifi_sta();
 	ESP_ERROR_CHECK(esp_wifi_init(&config));
 	ESP_ERROR_CHECK(esp_wifi_get_mac(ESP_IF_WIFI_STA, mac));
 	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
@@ -212,8 +203,6 @@ void wifi_start(wifi_credentials_t *ap_list, int size)
 	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 	ESP_ERROR_CHECK(esp_wifi_start());
 	init_done = true;
-
-	xTaskCreate(monitor, __FILE__, 2048, NULL, tskIDLE_PRIORITY + 1, 0);
 }
 
 bool wifi_is_connected(void)
@@ -244,4 +233,14 @@ const uint8_t *wifi_get_mac(void)
 const char *wifi_get_ip(void)
 {
 	return ip_address;
+}
+
+void wifi_set_hostname(const char *new_name)
+{
+	if (!sta_netif)
+		sta_netif = esp_netif_create_default_wifi_sta();
+
+	ESP_ERROR_CHECK(
+		esp_netif_set_hostname(sta_netif, new_name));
+	tag = (char *)new_name;
 }
